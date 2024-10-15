ab14d3ebf3c59a9791a5b30bfcb824ea98a72a0a
kuttapa <arrliver@gmail.com>
2024-10-15 17:10:06 +0700

    test: ci/cd pipeline was changed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/',(req,res)=>[
     res.status(200).json({
         status:200,
-        message:"Hello World"
+        message:"ci/cd pipeline was changed"
     })
 ])
 
